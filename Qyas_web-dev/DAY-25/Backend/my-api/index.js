219acba8a5a820f6ab9142552bf4d9cb91f0d18b
Daniel Teka <286605870+Daniel-Teka@users.noreply.github.com>
2026-06-23 08:36:01 +0000

    25ath

--- Qyas_web-dev/DAY-25/Backend/my-api/index.js
+++ Qyas_web-dev/DAY-25/Backend/my-api/index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Server is running smoothly!');
+  res.send('Server is running smoothly index!');
 });
 
 app.listen(PORT, () => {
